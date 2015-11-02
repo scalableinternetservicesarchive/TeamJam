2eb2b4768ce3064d99d1738e11914850cf78fe7c
Yanzhe Liu <nickliuyanzhe@gmail.com>
2015-11-01 23:23:23 -0800

    Minor fix for run_me. Now database password is added

--- run_me_for_nitrous.sh
+++ run_me_for_nitrous.sh
@@ -1,3 +1,6 @@
 line_old='  socket: /tmp/mysql.sock'
 line_new='  socket: /var/run/mysqld/mysqld.sock'
 sed -i "" "s|$line_old|$line_new|g" config/database.yml
+line_old='  password:'
+line_new='  password: oesophagus'
+sed -i "" "s|$line_old|$line_new|g" config/database.yml
