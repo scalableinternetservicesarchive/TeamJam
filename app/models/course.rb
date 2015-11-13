e33cd916d77532b34806b0adf566ab5b1c7dac0d
Yue Luo <leoyue94@gmail.com>
2015-11-13 00:44:54 -0800

    add not-nil constraint to new course creation

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,6 +5,8 @@ class Course < ActiveRecord::Base
   has_many :course_skillsets, dependent: :destroy
   has_many :skills, :through => :course_skillsets
   has_one :instructor, :class_name => 'User', :foreign_key => 'instructor_id'
+  validates :min_time_commitment, presence: true
+  validates :max_members, presence: true
 end
 
   
