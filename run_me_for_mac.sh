93b46d2a7479ff14683c8fccf1353c113bb466d7
Evan <gezhiliu942015@gmail.com>
2015-12-01 22:47:22 -0800

    recommit loadtest.xml

--- run_me_for_mac.sh
+++
@@ -1,6 +0,0 @@
-line_old='  socket: /var/run/mysqld/mysqld.sock'
-line_new='  socket: /tmp/mysql.sock'
-sed -i "" "s|$line_old|$line_new|g" config/database.yml
-line_old='  password: oesophagus'
-line_new='  password:'
-sed -i "" "s|$line_old|$line_new|g" config/database.yml
